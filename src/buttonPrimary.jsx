ae51a96727c7ca9942d238a482f3145291cc7647
SharpScratchy <sharp.scratchy@protonmail.com>
2021-05-13 00:01:26 +0200

    ✨ Implement a ButtonPrimary

---
+++ src/buttonPrimary.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "./button";
+
+const ButtonPrimary = (props) => {
+  return <Button classNameSuffix="primary" {...props} />
+};
+
+export default ButtonPrimary;
